13e006001417b48953fa2e717d988b3d3b487fc0
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-02-08 14:23:07 -0600

    Section 11: 143 Complete

--- DOM/index.js
+++ DOM/index.js
@@ -44,3 +44,30 @@ document.querySelector("input").click(); //searches entire document for input, t
 // - .appendChild()
 // - .setAttribute()
 // ------------------------------------------------------------------------------
+
+document.getElementsByTagName("li")[2].style.color = "orange"; //selects list item, [2] selects the third
+//list item (0, 1, 2), then we change the color of the third list item to orange.
+
+document.getElementsByTagName("li").length; //output = 3
+
+document.getElementsByClassName("btn")[0].style.color = "red"; //you have to use the square brackets
+//if you want to select the first of that class, even if there is juts ONE element with that class name
+
+document.getElementById("title").innerHTML = "New Title";
+
+//Query Selector will bring back tag, class or id, it doesn't matter which you pick!
+
+document.querySelector("h1"); //tag
+document.querySelector("#title"); //id
+document.querySelector(".btn"); //class
+
+document.querySelector("li a").style.color = "green"; //styles the link *inside* the li tag
+document.querySelector("li.item").style.color = "blue"; //styles the li with a class of item
+
+// document.querySelector("#list a").style.color = "pink"; //styles the first anchor tag in id of "list"
+
+//document.querySelector("#list .item") //only gives you back the first element with a class of .item inside id of "list",
+//even if there are multiple .item elements inside of "list"
+
+document.querySelectorAll("#list .item")[1].style.color = "red"; //selects all elements with class of item inside of
+//id of list, [1] grabs the second element and turns it red
