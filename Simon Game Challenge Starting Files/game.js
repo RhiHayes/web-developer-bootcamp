ee6255b1f53a9639d46283e0ef5e945d55933457
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-02-15 20:16:37 -0600

    Completed Simon Game

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,13 +1,16 @@
 
-var buttonColors = ["red", "blue", "yellow", "green"]; //simon says colors
+var buttonColors = ["red", "blue", "yellow", "green"]; //simon colors
 
 gamePattern = []; //the actual game pattern is stored here
-userClickedPattern = []; //the player's oattern is stored here
+userClickedPattern = []; //the player's pattern is stored here
 var started = false; // Game is not started by default
 var level = 0; //Level is at 0
 
 
 function nextSequence() { //determines the game's next move
+
+userClickedPattern = []; //Resetting user clicked pattern; IMPORTANT!!!
+//If you remove this, the functionality of simon doesn't work as intended.
 
   level++; //increments level
   $("#level-title").text("Level " + level); //updates level
@@ -15,7 +18,7 @@ function nextSequence() { //determines the game's next move
   var randomNum = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNum];
 
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100); //Adding animation to specified button
 
   playSound(randomChosenColor);
   animatePress(randomChosenColor);
@@ -63,5 +66,57 @@ $(".btn").on("click", function() { //tracks player's choice
   console.log(userChosenColor);
   userClickedPattern.push(userChosenColor);
 
-  console.log(userClickedPattern);
+  console.log(userClickedPattern)
+
+  checkAnswer(userClickedPattern.length-1); //checking most recent addition to array
 });
+
+
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    console.log("wrong");
+
+      $("#level-title").text("Game Over! Press any key to restart."); //updates level
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function(){
+          $("body").removeClass("game-over");
+     }, 200);
+
+     startOver();
+
+  }
+
+}
+
+function startOver() {
+
+  $(document).keydown(function() {
+
+    gamePattern = []; //the actual game pattern is stored here
+    started = false; // Game is not started by default
+    level = 0; //Level is at 0
+
+    nextSequence();
+
+  });
+
+}
