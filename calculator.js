d6decd9ca3cde2d4e22fe534e6d76e440f0701e6
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-02-25 00:21:32 -0600

    Section 20: 235-243 Completed

--- calculator.js
+++ calculator.js
@@ -16,11 +16,27 @@ var num2 = Number(req.body.num2);
 
 var result = num1 + num2;
 
-res.send("The result is: " + result);
+  res.send("The result is: " + result);
 
-  res.send("Thanks for posting!");
 })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
+})
+
+// BMI Calculator code below
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function(req, res) {
+
+var weight = parseFloat(req.body.weight);
+var height = parseFloat(req.body.height);
+
+var bmi = weight / (height * height);
+
+  res.send("Your BMI is: " + bmi);
+
 })
