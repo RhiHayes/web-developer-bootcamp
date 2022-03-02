aaae63df6222ed234bcdcf7e10a0e765bcca09da
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-03-02 00:16:42 -0600

    Finishing up Newsletter Project

--- app.js
+++ app.js
@@ -1,29 +1,72 @@
-
-const express = require("express");
+const express= require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-
+const request =  require("request");
+const https = require("https");
 const app = express();
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("Public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/signup.html");
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
 });
 
-app.post("/", function(req, res) {
 
-var firstName = req.body.fName;
-var lastName = req.body.lName;
-var email = req.body.email;
+app.post("/", function(req,res){
 
-console.log(firstName);
-console.log(lastName);
-console.log(email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+
+        }
+      }
+    ]
+  };
+
+  const jsonData =  JSON.stringify(data);
+
+  const url= "https://us14.api.mailchimp.com/3.0/lists/f2e13a5a2e";
+
+  const options = {
+    method: "POST",
+    auth: "bilale1:4ac86684790c14ea214254d26a1c2ab9-us14"
+  };
+
+  const request = https.request(url, options, function(response){
+
+    if (response.statusCode ===200) {
+      res.sendFile(__dirname+"/success.html");
+    } else {
+      res.sendFile(__dirname+"/failure.html");
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 
 });
 
-app.listen(3000, function() {
-  console.log("server is running on port 3000.");
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
 });
+
+
+// // 4ac86684790c14ea214254d26a1c2ab9-us14
+// // f2e13a5a2e
