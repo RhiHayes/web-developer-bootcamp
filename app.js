5581723afa6a2ee2a9ce568d4efe3809709f287e
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-03-03 00:33:47 -0600

    Updated Newsletter Project

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const request =  require("request");
 const https = require("https");
 const app = express();
 
-app.use(express.static("Public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
-  res.sendFile(__dirname+"/signup.html");
+  res.sendFile(__dirname+ "/signup.html");
 });
 
 
@@ -63,8 +63,8 @@ app.post("/failure",function(req,res){
   res.redirect("/");
 })
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000.");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running.");
 });
 
 
