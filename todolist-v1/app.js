9b9ec794af23fc4cefa8a86a5d671050f0d66b8f
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-03-08 00:20:05 -0600

    Working on Todo List

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,24 +4,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-  var items = ["Buy Food", "Cook Food", "Eat Food"];
+  let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let today = new Date();
+  let currentDay = today.getDay();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {kindOfDay: day, newListItems: items}); //Render in the get, not the post
 
@@ -29,7 +30,7 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 
