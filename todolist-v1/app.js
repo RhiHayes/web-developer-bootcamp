68ea444c5b169032f26f4520453bc11871b6f3a6
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-03-09 09:51:41 -0600

    Finished Todo List

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-  let items = ["Buy Food", "Cook Food", "Eat Food"];
+  const items = ["Buy Food", "Cook Food", "Eat Food"];
+  const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,30 +15,43 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
-  let currentDay = today.getDay();
+  const day = date.getDate(); //my module
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {kindOfDay: day, newListItems: items}); //Render in the get, not the post
+  res.render("list", {listTitle: day, newListItems: items}); //Render in the get, not the post
 
 });
 
 app.post("/", function(req, res) {
 
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
-  items.push(item);
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work"); //Refreshes WORK page once item is submitted
+  } else {
+    items.push(item);
+    res.redirect("/"); //Refreshes page once item is submitted
+  }
 
-  res.redirect("/"); //Refreshes page once item is submitted
 
 })
+
+app.get("/work", function(req, res) {
+
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+
+})
+
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
 
 app.listen(3000, function() {
   console.log("server started on port 3000.")
