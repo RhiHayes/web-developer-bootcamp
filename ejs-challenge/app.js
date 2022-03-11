4aaf25f829747f9547fd58d684100e60deab25a9
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-03-11 15:02:15 -0600

    Finished Blog Site

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const lodash = require("lodash");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -15,10 +16,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//Global Variables
+
+  const posts = [];
+
 //Home Route
 
 app.get("/", function(req, res) {
-  res.render("home", {homeContent: homeStartingContent}); //Setting homeContent equal to homeStartingContent variable
+  res.render("home",
+  {homeContent: homeStartingContent,
+  posts: posts}); //Setting homeContent equal to homeStartingContent variable
 })
 
 //About Route
@@ -40,8 +47,34 @@ app.get("/compose", function(req, res) {
 })
 
 app.post("/compose", function(req, res) {
-    const post = req.body.newPost;
-    console.log(post);
+
+    const post = {
+      title: req.body.postTitle,
+      body: req.body.postBody
+    }
+
+    posts.push(post);
+    res.redirect("/");
+
+})
+
+app.get("/posts/:postName", function(req, res) {
+  const requestedTitle = lodash.lowerCase(req.params.postName);
+
+
+
+  posts.forEach(function(post) {
+
+    const storedTitle = lodash.lowerCase(post.title);
+
+      if(storedTitle === requestedTitle) {
+        res.render("post",
+        {singleTitle: post.title,
+        singleBody: post.body});
+      }
+
+  })
+
 })
 
 app.listen(3000, function() {
