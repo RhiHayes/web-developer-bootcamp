9939b05441e5ecb6fa176bea2404a446859b4f10
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-04-15 00:40:19 -0500

    React Devtools

---
+++ React Practice/react-devtools-forked/src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQEXZr19-jmZGg/profile-displayphoto-shrink_200_200/0/1623468711576?e=1654732800&v=beta&t=cKd6jK_TG7sP_sU4btctMBkwfwyRCbQSQWf1NRQnEek" />
+
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
